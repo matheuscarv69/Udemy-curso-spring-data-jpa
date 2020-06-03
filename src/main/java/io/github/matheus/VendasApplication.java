1c7823169419987d291dce13d6624e8226005b88
matheuscarv69 <matheus9126@gmail.com>
2020-06-03 16:54:15 -0400

    jpa configuration and h2 database

--- src/main/java/io/github/matheus/VendasApplication.java
+++ src/main/java/io/github/matheus/VendasApplication.java
@@ -1,14 +1,7 @@
 package io.github.matheus;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class VendasApplication {
