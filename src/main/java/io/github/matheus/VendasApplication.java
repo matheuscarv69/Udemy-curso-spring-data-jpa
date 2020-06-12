d3aa0998ab0228d9aa70370ffe9acb165fc541a8
Matheus Carvalho <matheus9126@gmail.com>
2020-06-12 15:54:12 -0400

    loading orders by customer and finalizing section 3

--- src/main/java/io/github/matheus/VendasApplication.java
+++ src/main/java/io/github/matheus/VendasApplication.java
@@ -1,30 +1,45 @@
 package io.github.matheus;
 
 import io.github.matheus.domain.entity.Cliente;
+import io.github.matheus.domain.entity.Pedido;
 import io.github.matheus.domain.repository.Clientes;
+import io.github.matheus.domain.repository.Pedidos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 @SpringBootApplication
 public class VendasApplication {
 
     @Bean
-    public CommandLineRunner init(@Autowired Clientes clientes) {
+    public CommandLineRunner init(
+            @Autowired Pedidos pedidos,
+            @Autowired Clientes clientes) {
         return args -> {
 
             System.out.println("Salvando Clientes");
-            clientes.save(new Cliente("Matheus Carvalho"));
-            clientes.save(new Cliente("Teuzin dus vrauuu"));
+            Cliente fulano = new Cliente("Fulano");
+            clientes.save(fulano);
 
-            List<Cliente> result = clientes.encontrarPorNome("Matheus Carvalho");
-            result.forEach(System.out::println);
+            Pedido p = new Pedido();
+            p.setCliente(fulano);
+            p.setDataPedido(LocalDate.now());
+            p.setTotal(BigDecimal.valueOf(100));
 
+            pedidos.save(p);
 
+//            Cliente cliente = clientes.findClienteFetchPedidos(fulano.getId());
+//            System.out.println(cliente);
+//            System.out.println(cliente.getPedidos());
+
+            List<Pedido> listaPedidos = pedidos.findByCliente(fulano);
+            listaPedidos.forEach(System.out::println);
 
         };
     }
--- src/main/java/io/github/matheus/domain/repository/Pedidos.java
+++ src/main/java/io/github/matheus/domain/repository/Pedidos.java
@@ -1,8 +1,12 @@
 package io.github.matheus.domain.repository;
 
+import io.github.matheus.domain.entity.Cliente;
 import io.github.matheus.domain.entity.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface Pedidos extends JpaRepository<Pedido, Integer> {
 
+    List<Pedido> findByCliente(Cliente cliente);
 }
