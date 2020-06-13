6e6f5ac7789bfb88d7d4161c966acf0fdd86f484
Matheus Carvalho <matheus9126@gmail.com>
2020-06-13 16:09:12 -0400

    Section 4: GET Request and Response Entity at the Customer entity

--- src/main/java/io/github/matheus/VendasApplication.java
+++ src/main/java/io/github/matheus/VendasApplication.java
@@ -1,50 +1,32 @@
 package io.github.matheus;
 
 import io.github.matheus.domain.entity.Cliente;
-import io.github.matheus.domain.entity.Pedido;
 import io.github.matheus.domain.repository.Clientes;
-import io.github.matheus.domain.repository.Pedidos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.List;
-
 @SpringBootApplication
 public class VendasApplication {
 
     @Bean
-    public CommandLineRunner init(
-            @Autowired Pedidos pedidos,
-            @Autowired Clientes clientes) {
+    public CommandLineRunner commandLineRunner(@Autowired Clientes clientes){
         return args -> {
+            Cliente c = new Cliente(null, "Fulano");
 
-            System.out.println("Salvando Clientes");
-            Cliente fulano = new Cliente("Fulano");
-            clientes.save(fulano);
+            clientes.save(c);
 
-            Pedido p = new Pedido();
-            p.setCliente(fulano);
-            p.setDataPedido(LocalDate.now());
-            p.setTotal(BigDecimal.valueOf(100));
-
-            pedidos.save(p);
-
-//            Cliente cliente = clientes.findClienteFetchPedidos(fulano.getId());
-//            System.out.println(cliente);
-//            System.out.println(cliente.getPedidos());
-
-            List<Pedido> listaPedidos = pedidos.findByCliente(fulano);
-            listaPedidos.forEach(System.out::println);
 
         };
+
     }
+
+
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
+
     }
 }
---
+++ src/main/java/io/github/matheus/rest/controller/ClienteController.java
@@ -0,0 +1,32 @@
+package io.github.matheus.rest.controller;
+
+import io.github.matheus.domain.entity.Cliente;
+import io.github.matheus.domain.repository.Clientes;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@Controller
+public class ClienteController {
+
+    private Clientes clientes;
+
+    public ClienteController(Clientes clientes) {
+        this.clientes = clientes;
+    }
+
+    @GetMapping("/api/clientes/{id}")
+    @ResponseBody
+    public ResponseEntity getClienteById(@PathVariable Integer id) {
+        Optional<Cliente> cliente = clientes.findById(id);
+
+        if (cliente.isPresent() ) {
+            return ResponseEntity.ok(cliente.get());
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+}
+
